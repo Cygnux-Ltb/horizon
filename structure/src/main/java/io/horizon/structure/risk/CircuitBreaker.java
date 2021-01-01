3eb3c8aafcae2d9d2bff7f51331441eb39e13d1c
yellow013 <wk9988@gmail.com>
2021-01-02 00:00:18 +0800

    update risk

--- structure/src/main/java/io/horizon/structure/risk/CircuitBreaker.java
+++ structure/src/main/java/io/horizon/structure/risk/CircuitBreaker.java
@@ -2,12 +2,32 @@ package io.horizon.structure.risk;
 
 public interface CircuitBreaker {
 
+	/**
+	 * 启用账户
+	 * 
+	 * @param accountId
+	 */
 	void enableAccount(int accountId);
 
+	/**
+	 * 关闭账户
+	 * 
+	 * @param accountId
+	 */
 	void disableAccount(int accountId);
 
+	/**
+	 * 启用标的
+	 * 
+	 * @param instrumentId
+	 */
 	void enableInstrument(int instrumentId);
 
+	/**
+	 * 关闭标的
+	 * 
+	 * @param instrumentId
+	 */
 	void disableInstrument(int instrumentId);
 
 }
