d4653709f5450957e46113bf8b1d754fdb84faf4
yellow013 <wk9988@gmail.com>
2021-05-26 23:55:26 +0800

    update ftdc service

--- adaptor-ftdc-service/src/main/java/io/horizon/ftdc/service/FtdcServiceController.java
+++ adaptor-ftdc-service/src/main/java/io/horizon/ftdc/service/FtdcServiceController.java
@@ -1,11 +1,19 @@
 package io.horizon.ftdc.service;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/ftdc")
 public class FtdcServiceController {
+
+	private final AtomicBoolean isLogin = new AtomicBoolean(false);
+	
+	//private final AtomicReference<Ftdcparam>
+	
 
 	@RequestMapping("/")
 	public String index(Model model) {
