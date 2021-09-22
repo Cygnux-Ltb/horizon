ae1fdcc6f56bfd26fb3bb06c96b938f9aa1f71be
yellow013 <wk9988@gmail.com>
2021-09-22 23:42:41 +0800

    update binance

--- adaptor-binance/src/main/java/io/horizon/adaptor/binance/AsyncHttpClientWebsocket.java
+++ adaptor-binance/src/main/java/io/horizon/adaptor/binance/AsyncHttpClientWebsocket.java
@@ -1,13 +1,56 @@
 package io.horizon.adaptor.binance;
 
+import static org.asynchttpclient.Dsl.*;
+
+import java.util.concurrent.ExecutionException;
+
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.ws.WebSocket;
+import org.asynchttpclient.ws.WebSocketListener;
+import org.asynchttpclient.ws.WebSocketUpgradeHandler;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class AsyncHttpClientWebsocket {
+	public static void main(String[] args) {
+
+		AsyncHttpClient c = asyncHttpClient();
+
+		try {
+			WebSocket websocket = c.prepareGet("ws://demos.kaazing.com/echo")
+					.execute(new WebSocketUpgradeHandler.Builder().addWebSocketListener(new WebSocketListener() {
+
+						@Override
+						public void onOpen(WebSocket websocket) {
+							websocket.sendTextFrame("...");
+						}
+
+						@Override
+						public void onClose(WebSocket websocket, int code, String reason) {
+							// TODO Auto-generated method stub
+
+						}
+
+						@Override
+						public void onTextFrame(String payload, boolean finalFragment, int rsv) {
+							System.out.println(payload);
+						}
+
+						@Override
+						public void onError(Throwable t) {
+						}
+
+					}).build()).get();
+
+			System.out.println(websocket);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
