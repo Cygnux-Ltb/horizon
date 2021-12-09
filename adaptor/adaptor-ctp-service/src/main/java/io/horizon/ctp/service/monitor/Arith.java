af15deb1904bf8fb386108a87443a7e48258720e
yellow013 <wk9988@gmail.com>
2021-12-09 23:33:47 +0800

    update adaptor pom.xml

--- adaptor/adaptor-ctp-service/src/main/java/io/horizon/ctp/service/monitor/Arith.java
+++ adaptor/adaptor-ctp-service/src/main/java/io/horizon/ctp/service/monitor/Arith.java
@@ -75,14 +75,12 @@ public class Arith {
 	 * @return 两个参数的商
 	 */
 	public static double div(double v1, double v2, int scale) {
-		if (scale < 0) {
+		if (scale < 0)
 			throw new IllegalArgumentException("The scale must be a positive integer or zero");
-		}
 		BigDecimal b1 = new BigDecimal(Double.toString(v1));
 		BigDecimal b2 = new BigDecimal(Double.toString(v2));
-		if (b1.compareTo(BigDecimal.ZERO) == 0) {
+		if (b1.compareTo(BigDecimal.ZERO) == 0)
 			return BigDecimal.ZERO.doubleValue();
-		}
 		return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
 	}
 
@@ -94,9 +92,8 @@ public class Arith {
 	 * @return 四舍五入后的结果
 	 */
 	public static double round(double v, int scale) {
-		if (scale < 0) {
+		if (scale < 0)
 			throw new IllegalArgumentException("The scale must be a positive integer or zero");
-		}
 		BigDecimal b = new BigDecimal(Double.toString(v));
 		BigDecimal one = new BigDecimal("1");
 		return b.divide(one, scale, RoundingMode.HALF_UP).doubleValue();
--- adaptor/adaptor-fix4j/src/main/java/io/horizon/App.java
+++ adaptor/adaptor-fix4j/src/main/java/io/horizon/App.java
@@ -4,10 +4,10 @@ package io.horizon;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
